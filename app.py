962c63a8b425b1fb7fd51594296236dbb6a89491
Subhojit Sinha <subhojitsinha7@gmail.com>
2024-05-25 13:25:51 +0530

    adding blueprint route

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
 from flask import Flask, jsonify
-import pymongo
-from pymongo import MongoClient
+from routes import bp_routes
 
 app = Flask(__name__)
 
+# Registering the blueprint
+app.register_blueprint(bp_routes)
 
-def get_db():
-    client = MongoClient(
-        host='test_mongodb',
-        port=27017,
-        username='root',
-        password='pass',
-        authSource='admin'
-    )
-    return client['animal_db']
 
-@app.route('/')
-def ping_server():
-    return "hello world"
 
-@app.route('/animals')
-def fetch_animals():
-    db = get_db()
-    _animals = db.animal_tb.find()
-    animals = [{"id":animal["id"], "name":animal["name"], "type":animal["type"] } for animal in _animals]
-    # return jsonify(list(_animals))
-    return jsonify({"animals": animals})
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
