ffc0d22a437a6b9d63248fd887f3e67a4c9cf277
Subhojit Sinha <subhojitsinha7@gmail.com>
2024-05-31 18:05:49 +0530

    adding basemodel and related changes

--- controllers/test.py
+++ controllers/test.py
@@ -3,10 +3,13 @@ from flask import jsonify
 from models.Models import Account, User, RequestLogs, SchedulerData, BaseModel
 from configs.database import DB
 from bson import ObjectId
+import random
+
 def hello_world():
     return '<h1>Hello World</h1>'
 
-def init_database():
+def insert_test():
+    print("Start")
     account = Account()
     id = account.insert([{
         'name'    : "Avijit",
@@ -17,12 +20,15 @@ def init_database():
         'email'   : "Email2",
         'password': "TestPassword"
     }])
+
     print(id)
+    print("End")
     return str(id)
 
-def get_all():
+def find_all():
     account = Account()
-    data = account.get_all()
+    data = account.find_all({"name": "kalu"})
+    print(data)
     return jsonify(data)
 
 def get_all_3():
@@ -66,13 +72,13 @@ def insert_all():
         'email'   : "Email2",
         'password': "TestPassword"
     }]
-
-    return {
-        'id1' : Account().insert(data),
-        'id2' : User().insert(data),
-        'id3' : RequestLogs().insert(data),
-        'id4' : SchedulerData().insert(data)
-    }
+    data = {}
+    return str({
+        'id1' : Account().count(data),
+        'id2' : User().count(data),
+        'id3' : RequestLogs().count(data),
+        'id4' : SchedulerData().count(data)
+    })
 
 def test_query():
     # DB        = BaseModel('accounts')
@@ -86,3 +92,35 @@ def test_query():
         return_data.append(field)
 
     return str(return_data)
+
+def test_q():
+    data = Account().delete_by_id("66582377a5d873d8b0b1e6e3")
+    print(type(data))
+    return str(data)
+
+def insert_test_thread():
+    print("Start")
+    account = Account()
+    id = account.insert_on_thread([{
+        'name'    : "Avijit",
+        'email'   : "TestEmailNew",
+        'password': "TestPasswordXX"
+    },{
+        'name'    : "Subhojit",
+        'email'   : "Email2",
+        'password': "TestPassword"
+    }])
+
+    print("End")
+    return f"insert_on_thread: {random.randint(1, 10)}"
+
+def update_test_thread():
+    print("Start")
+    Account().update_on_thread({'name':"Subhojit"},{'name':"Subhojit Sinha"})
+    print("End")
+    return f"update_test_thread: {random.randint(1, 10)}"
+def delete_test_thread():
+    print("Start")
+    Account().delete_on_thread({'name':"Avijit"})
+    print("End")
+    return f"delete_on_thread: {random.randint(1, 10)}"
--- routes/routes.py
+++ routes/routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
-from controllers.test import hello_world,init_database,get_all,get_all_3,find_name,update_name,delete_doc,count_all,distint_by_field,insert_all,test_query
+from controllers.test import *
 bp_routes = Blueprint('blueprint', __name__)
 
 bp_routes.route('/', methods=['GET'])(hello_world)
-bp_routes.route('/init-database', methods=['GET'])(init_database)
-bp_routes.route('/get-all', methods=['GET'])(get_all)
+bp_routes.route('/insert-test', methods=['GET'])(insert_test)
+bp_routes.route('/get-all', methods=['GET'])(find_all)
 bp_routes.route('/get-all-3', methods=['GET'])(get_all_3)
 bp_routes.route('/find-name/<name>', methods=['GET'])(find_name)
 bp_routes.route('/update-name/<id>/<name>', methods=['GET'])(update_name)
@@ -13,3 +13,8 @@ bp_routes.route('/count-all', methods=['GET'])(count_all)
 bp_routes.route('/distinct/<field>', methods=['GET'])(distint_by_field)
 bp_routes.route('/insert-all', methods=['GET'])(insert_all)
 bp_routes.route('/test-query', methods=['GET'])(test_query)
+bp_routes.route('/test-q', methods=['GET'])(test_q)
+
+bp_routes.route('/insert-test-thread', methods=['GET'])(insert_test_thread)
+bp_routes.route('/update-test-thread', methods=['GET'])(update_test_thread)
+bp_routes.route('/delete-test-thread', methods=['GET'])(delete_test_thread)
